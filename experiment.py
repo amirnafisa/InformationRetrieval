c610b056f47ae41c496981b588b034209e2df5b4
Nafisa <amirnafisa@gmail.com>
2019-04-22 14:13:33 -0400

    1. Updated data structure. Saving states regularly saving time.\n2. Using hw3 default classifier model for training. Can be updated to use EM, KNeighbour etc or other sklearn classifier.\n3.Created output file.\n\nNext steps would be to compute precision and recall.

--- experiment.py
+++ experiment.py
@@ -1,57 +1,115 @@
 from misc import *
+from sklearn.linear_model import LogisticRegression
 
 if __name__ == '__main__':
 
     task1_n_train = 100
     task1_n_test = 300
 
-    #Load Noun Phrases if already extracted from the training docs for task1
-    noun_phrases = tmp_load('noun_phrases')
+    print("Reading Task1 Docs ...")
     vocab = tmp_load('vocab')
-    if not noun_phrases:
+    train_docs_tokens_in = tmp_load('train_docs_tokens_in')
 
-        #Extract Noun phrases from the training docs for task1
-        noun_phrases = defaultdict(int)
+    if not vocab or not train_docs_tokens_in:
+        train_docs_tokens_in, vocab = read_docs(0, task1_n_train, task=1, train_mode='Train', doc_type='docs')
+        tmp_save(vocab,'vocab')
+        tmp_save(train_docs_tokens_in, 'train_docs_tokens_in')
 
-        docs_tokens, vocab = read_docs(task1_n_train, task=1, train_mode='Train', doc_type='docs')
+    train_docs_tokens_ot = tmp_load('train_docs_tokens_ot')
+    if not train_docs_tokens_ot:
+        train_docs_tokens_ot, _     = read_docs(0, task1_n_train, task=1, train_mode='Train', doc_type='catches')
+        tmp_save(train_docs_tokens_ot, 'train_docs_tokens_ot')
+
+    test_docs_tokens_in = tmp_load('test_docs_tokens_in')
+    if not test_docs_tokens_in:
+        test_docs_tokens_in, _      = read_docs(task1_n_train, task1_n_test, task=1, train_mode='Test', doc_type='docs')
+        tmp_save(test_docs_tokens_in, 'test_docs_tokens_in')
+
+    test_docs_tokens_ot = tmp_load('test_docs_tokens_ot')
+    if not test_docs_tokens_ot:
+        test_docs_tokens_ot, _      = read_docs(task1_n_train, task1_n_test, task=1, train_mode='Test', doc_type='catches')
+        tmp_save(test_docs_tokens_ot, 'test_docs_tokens_ot')
+
+    print("Extracting noun phrases ...")
+
+    train_X_doc         = tmp_load('train_X_doc')
+    train_set_of_NP_X   = tmp_load('train_set_of_NP_X')
+    train_Y_doc         = tmp_load('train_Y_doc')
+    train_set_of_NP_Y   = tmp_load('train_set_of_NP_Y')
+    test_X_doc          = tmp_load('test_X_doc')
+    test_set_of_NP_X    = tmp_load('test_set_of_NP_X')
+
+    if not train_X_doc:
+        train_X_doc = defaultdict()
+        train_Y_doc = defaultdict()
+        test_X_doc  = defaultdict()
+
+        train_set_of_NP_X = set()
+        train_set_of_NP_Y = set()
+        test_set_of_NP_X  = set()
+
 
         for i in range(task1_n_train):
-            noun_phrases = get_noun_phrases(docs_tokens[i], noun_phrases)
+            train_X_doc[i] = set(get_noun_phrases(train_docs_tokens_in[i]).keys())
+            train_set_of_NP_X.update(train_X_doc[i])
+            train_Y_doc[i] = set(get_noun_phrases(train_docs_tokens_ot[i]).keys())
+            train_set_of_NP_Y.update(train_Y_doc[i])
 
-        tmp_save(noun_phrases,'noun_phrases')
-        tmp_save(vocab, 'vocab')
+        for i in range(task1_n_test):
+            test_X_doc[i] = set(get_noun_phrases(test_docs_tokens_in[i]).keys())
+            test_set_of_NP_X.update(test_X_doc[i])
 
-    #Create vectors for training for task 1
-    X = get_vector_embedding_for_NPs(vocab, noun_phrases.keys()) #Option1 ignoring the frequency of NPs
+        #Convert from unordered to ordered collection
+        train_set_of_NP_X = list(train_set_of_NP_X)
+        train_set_of_NP_Y = list(train_set_of_NP_Y)
+        test_set_of_NP_X  = list(test_set_of_NP_X )
 
-    #Load golden phrases if already saved
-    golden_phrases = tmp_load('golden_phrases')
+        tmp_save(train_X_doc, 'train_X_doc')
+        tmp_save(train_set_of_NP_X, 'train_set_of_NP_X')
+        tmp_save(train_Y_doc, 'train_Y_doc')
+        tmp_save(train_set_of_NP_Y, 'train_set_of_NP_Y')
+        tmp_save(test_X_doc, 'test_X_doc')
+        tmp_save(test_set_of_NP_X, 'test_set_of_NP_X')
 
-    if not golden_phrases:
-        #Extract Golden phrases from the training catches for task1
-        golden_phrases = set()
+    print("Creating vector embeddings ...")
 
-        docs_tokens, _ = read_docs(task1_n_train, task=1, train_mode='Train', doc_type='catches')
+    X_train = tmp_load('X_train')
+    if type(X_train).__name__ == 'NoneType':
+        X_train = get_vector_embedding_for_NPs(vocab, train_set_of_NP_X) #Option1 ignoring the frequency of NPs
+        tmp_save(X_train, 'X_train')
 
-        for i in range(task1_n_train):
-            golden_phrases.update(docs_tokens[i])
+    X_test = tmp_load('X_test')
+    if type(X_test).__name__ == 'NoneType':
+        X_test  = get_vector_embedding_for_NPs(vocab, test_set_of_NP_X ) #Option1 ignoring the frequency of NPs
+        tmp_save(X_test, 'X_test')
 
-        tmp_save(golden_phrases,'golden_phrases')
+    Y_train = tmp_load('Y_train')
+    if type(Y_train).__name__ == 'NoneType':
+        Y_train = np.zeros((X_train.shape[0],))
+        for i, phrase in enumerate(train_set_of_NP_X):
+            if phrase in train_set_of_NP_Y:
+                Y_train[i] = 1
+        tmp_save(Y_train, 'Y_train')
 
-    Y = np.zeros((X.shape[0],))
-    for i, phrase in enumerate(noun_phrases.keys()):
-        if phrase in golden_phrases:
-            Y[i] = 1
 
-
-    print(X.shape,Y.shape)
+    print("Training Task1 ...")
 
     #Start Training (X,Y)
+    Y_test_hat = tmp_load('Y_test_hat')
+    if type(Y_test_hat).__name__ == 'NoneType':
+        Y_test_hat = Default(X_train, X_test, Y_train)
+        tmp_save(Y_test_hat, 'Y_test_hat')
 
-    #Load Test Dataset and extract noun_phrases
+    print("Printing Task1 Outputs ...")
 
-    #Predict for noun phrases in test Dataset
+    f = open('tmp_files/output_task1.tsv','w')
+    for i in range(task1_n_test):
+        f.write(str(i)+':\t')
 
-    #Add the predictions in the golden file
+        for noun_phrase in test_X_doc[i]:
+            j = test_set_of_NP_X.index(noun_phrase)
 
-    #Evaluate by computing precision recall (Add functions)
+            if Y_test_hat[j] == 1:
+                f.write(str(noun_phrase)+', ')
+        f.write('\n')
+    f.close()
--- misc.py
+++ misc.py
@@ -3,7 +3,7 @@ import sys
 
 import nltk
 import numpy as np
-
+from numpy.linalg import norm
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk import pos_tag
 from gensim.models import Word2Vec
@@ -28,15 +28,17 @@ def get_file_name(task=1, train_mode='Train', doc_type='docs', idx=0):
     return os.path.join(proj_dir,filename)
 
 
-def read_docs(n_files, task=1, train_mode='Train', doc_type='docs'):
+def read_docs(start_idx, n_files, task=1, train_mode='Train', doc_type='docs'):
     '''
     Reads the corpus into a list of Documents
     '''
     docs = []
     vocab = []
 
+
     for i in range(n_files):
-        file = get_file_name(task, train_mode, doc_type, i)
+
+        file = get_file_name(task, train_mode, doc_type, start_idx+i)
 
         with open(file,encoding = "ISO-8859-1") as f:
             docs.append([])
@@ -53,11 +55,11 @@ def read_docs(n_files, task=1, train_mode='Train', doc_type='docs'):
     return docs, vocab
 
 
-def get_noun_phrases(doc_tokens, noun_phrases):
+def get_noun_phrases(doc_tokens):
     '''
     Reads the corpus into a list of Documents
     '''
-
+    noun_phrases = defaultdict(int)
     my_parser = nltk.RegexpParser (
             r"""
                 NP: {<DT|JJ|NN.*>+}          # Chunk sequences of DT, JJ, NN
@@ -67,9 +69,7 @@ def get_noun_phrases(doc_tokens, noun_phrases):
 
     word_tagged_text = pos_tag(doc_tokens)
     phrase_tagged_text = my_parser.parse(word_tagged_text)
-    if 'U.S.A' in word_tagged_text:
-        print(word_tagged_text)
-        sys.exit(-1)
+
     for subtree in phrase_tagged_text.subtrees(filter=lambda t: t.label()=='NP'):
         noun_phrases[' '.join(leaf[0] for leaf in subtree.leaves())] += 1
     for (word,tag) in filter(lambda tag: tag=='NN' or tag=='NNP' or tag=='NNS',word_tagged_text):
@@ -99,8 +99,7 @@ def get_vector_embedding_for_NPs(vocab, phrases, model=True):
     if model:
         model = get_model('cur_model.mdl')
     if not model:
-        if 'kurian' in vocab:
-            print("OOOOOOOO")
+
         model = Word2Vec(vocab, min_count=1, size=VEC_SIZE, window=5) #CBOW Model from gensim
         save_model(model, 'cur_model.mdl')
 
@@ -110,11 +109,36 @@ def get_vector_embedding_for_NPs(vocab, phrases, model=True):
     return doc_vecs
 
 
+def compute_centroid(vecs):
+    return np.mean(vecs,axis=0)
+
+
+def cosine_sim(x, y):
+    '''
+    Computes the cosine similarity between two sparse term vectors represented as dictionaries.
+    '''
+    if norm(x) == 0 or norm(y) == 0:
+        return 0
+    return np.dot(x.T,y)/(norm(x) * norm(y))
+
+
+def Default(doc_vecs_tr, doc_vecs_tt, Y_tr):
+    centroid_profile1 = compute_centroid(doc_vecs_tr[Y_tr==1])
+    centroid_profile2 = compute_centroid(doc_vecs_tr[Y_tr==0])
+
+    dif = np.array([cosine_sim(vec, centroid_profile1)-cosine_sim(vec, centroid_profile2) for vec in doc_vecs_tt])
+
+    Y_hat = np.ones_like(dif)
+    Y_hat[dif <= 0] = 0
+    return Y_hat
+
+
 def get_vector_embedding(model, phrase):
 
     vec = np.zeros((VEC_SIZE,))
     for word in phrase.split(' '):
-        vec += model.wv[word]
+        if word in model.wv:
+            vec += model.wv[word]
     vec /= len(phrase)
 
     return vec
